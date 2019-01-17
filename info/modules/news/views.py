b2eb6caa5fc46a07884e09709d8000a68df265d5
shengjun <371673381@qq.com>
2019-01-17 10:16:24 +0800

    项目目录搭建，导入静态文件夹，创建模板文件夹，加载项目首页

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,10 +1,17 @@
-from flask import session
+from flask import session,render_template
 # 导入蓝图对象
 from . import news_blue
 
 @news_blue.route("/")
 def index():
+    """
+    项目首页加载
+    1、使用模板加载项目首页----把static文件夹粘贴到info/目录下
+
+
+    :return:
+    """
     session['itcast'] = '2019'
-    return 'hello world2019'
+    return render_template('news/index.html')
 
 
